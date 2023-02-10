65cae864bafe36c628b9a0d8a80b4a5ebdf712b1
FelipeElizondo <124946900+FelipeElizondo@users.noreply.github.com>
2023-02-09 19:53:19 -0600

    Update ItemValues.java
    I only wanted this to make it easier

--- src/main/java/com/tristankechlo/toolleveling/config/values/ItemValues.java
+++ src/main/java/com/tristankechlo/toolleveling/config/values/ItemValues.java
@@ -52,86 +52,86 @@ public final class ItemValues extends RegistryMapConfig<Item, Long> {
         Map<Item, Long> values = new HashMap<>();
 
         // Ores
-        values.put(Items.COAL, 8L);
-        values.put(Items.COAL_ORE, 30L);
-        values.put(Items.DEEPSLATE_COAL_ORE, 30L);
-        values.put(Items.COAL_BLOCK, 73L);
+        values.put(Items.COAL, 80L);
+        values.put(Items.COAL_ORE, 300L);
+        values.put(Items.DEEPSLATE_COAL_ORE, 300L);
+        values.put(Items.COAL_BLOCK, 730L);
 
-        values.put(Items.COPPER_ORE, 10L);
-        values.put(Items.DEEPSLATE_COPPER_ORE, 10L);
-        values.put(Items.RAW_COPPER, 11L);
-        values.put(Items.COPPER_INGOT, 14L);
-        values.put(Items.COPPER_BLOCK, 126L);
-        values.put(Items.RAW_COPPER_BLOCK, 99L);
+        values.put(Items.COPPER_ORE, 100L);
+        values.put(Items.DEEPSLATE_COPPER_ORE, 100L);
+        values.put(Items.RAW_COPPER, 110L);
+        values.put(Items.COPPER_INGOT, 140L);
+        values.put(Items.COPPER_BLOCK, 1260L);
+        values.put(Items.RAW_COPPER_BLOCK, 990L);
 
-        values.put(Items.RAW_IRON, 13L);
-        values.put(Items.IRON_INGOT, 15L);
-        values.put(Items.IRON_ORE, 12L);
-        values.put(Items.DEEPSLATE_IRON_ORE, 12L);
-        values.put(Items.RAW_IRON_BLOCK, 117L);
-        values.put(Items.IRON_BLOCK, 135L);
+        values.put(Items.RAW_IRON, 130L);
+        values.put(Items.IRON_INGOT, 150L);
+        values.put(Items.IRON_ORE, 120L);
+        values.put(Items.DEEPSLATE_IRON_ORE, 120L);
+        values.put(Items.RAW_IRON_BLOCK, 1170L);
+        values.put(Items.IRON_BLOCK, 1350L);
 
-        values.put(Items.GOLD_INGOT, 40L);
-        values.put(Items.RAW_GOLD, 35L);
-        values.put(Items.GOLD_ORE, 30L);
-        values.put(Items.DEEPSLATE_GOLD_ORE, 30L);
-        values.put(Items.RAW_GOLD_BLOCK, 315L);
-        values.put(Items.GOLD_BLOCK, 360L);
+        values.put(Items.GOLD_INGOT, 400L);
+        values.put(Items.RAW_GOLD, 350L);
+        values.put(Items.GOLD_ORE, 300L);
+        values.put(Items.DEEPSLATE_GOLD_ORE, 300L);
+        values.put(Items.RAW_GOLD_BLOCK, 3150L);
+        values.put(Items.GOLD_BLOCK, 3600L);
 
-        values.put(Items.DIAMOND, 160L);
-        values.put(Items.DIAMOND_ORE, 160L);
-        values.put(Items.DEEPSLATE_DIAMOND_ORE, 160L);
-        values.put(Items.DIAMOND_BLOCK, 1450L);
-        values.put(Items.NETHERITE_INGOT, 200L);
-        values.put(Items.NETHERITE_SCRAP, 50L);
-        values.put(Items.ANCIENT_DEBRIS, 50L);
-        values.put(Items.NETHERITE_BLOCK, 1800L);
-        values.put(Items.LAPIS_LAZULI, 8L);
-        values.put(Items.LAPIS_ORE, 120L);
-        values.put(Items.DEEPSLATE_LAPIS_ORE, 120L);
-        values.put(Items.LAPIS_BLOCK, 70L);
-        values.put(Items.EMERALD, 100L);
-        values.put(Items.EMERALD_ORE, 800L);
-        values.put(Items.DEEPSLATE_EMERALD_ORE, 800L);
-        values.put(Items.EMERALD_BLOCK, 900L);
-        values.put(Items.QUARTZ, 10L);
-        values.put(Items.NETHER_QUARTZ_ORE, 40L);
-        values.put(Items.QUARTZ_BLOCK, 40L);
-        values.put(Items.REDSTONE, 4L);
-        values.put(Items.REDSTONE_ORE, 60L);
-        values.put(Items.DEEPSLATE_REDSTONE_ORE, 60L);
-        values.put(Items.REDSTONE_BLOCK, 36L);
-        values.put(Items.GLOWSTONE_DUST, 4L);
-        values.put(Items.GLOWSTONE, 15L);
+        values.put(Items.DIAMOND, 1600L);
+        values.put(Items.DIAMOND_ORE, 1600L);
+        values.put(Items.DEEPSLATE_DIAMOND_ORE, 1600L);
+        values.put(Items.DIAMOND_BLOCK, 14500L);
+        values.put(Items.NETHERITE_INGOT, 2000L);
+        values.put(Items.NETHERITE_SCRAP, 500L);
+        values.put(Items.ANCIENT_DEBRIS, 500L);
+        values.put(Items.NETHERITE_BLOCK, 18000L);
+        values.put(Items.LAPIS_LAZULI, 80L);
+        values.put(Items.LAPIS_ORE, 1200L);
+        values.put(Items.DEEPSLATE_LAPIS_ORE, 1200L);
+        values.put(Items.LAPIS_BLOCK, 700L);
+        values.put(Items.EMERALD, 1000L);
+        values.put(Items.EMERALD_ORE, 8000L);
+        values.put(Items.DEEPSLATE_EMERALD_ORE, 8000L);
+        values.put(Items.EMERALD_BLOCK, 9000L);
+        values.put(Items.QUARTZ, 100L);
+        values.put(Items.NETHER_QUARTZ_ORE, 400L);
+        values.put(Items.QUARTZ_BLOCK, 400L);
+        values.put(Items.REDSTONE, 40L);
+        values.put(Items.REDSTONE_ORE, 600L);
+        values.put(Items.DEEPSLATE_REDSTONE_ORE, 600L);
+        values.put(Items.REDSTONE_BLOCK, 360L);
+        values.put(Items.GLOWSTONE_DUST, 40L);
+        values.put(Items.GLOWSTONE, 150L);
 
         // other
-        values.put(Items.AMETHYST_BLOCK, 11L);
-        values.put(Items.AMETHYST_SHARD, 17L);
+        values.put(Items.AMETHYST_BLOCK, 110L);
+        values.put(Items.AMETHYST_SHARD, 170L);
 
         // Food
-        values.put(Items.GOLDEN_APPLE, 400L);
-        values.put(Items.GOLDEN_CARROT, 100L);
-        values.put(Items.GLISTERING_MELON_SLICE, 100L);
-        values.put(Items.ENCHANTED_GOLDEN_APPLE, 2500L);
+        values.put(Items.GOLDEN_APPLE, 4000L);
+        values.put(Items.GOLDEN_CARROT, 1000L);
+        values.put(Items.GLISTERING_MELON_SLICE, 1000L);
+        values.put(Items.ENCHANTED_GOLDEN_APPLE, 25000L);
 
         // Drops
-        values.put(Items.SLIME_BALL, 25L);
-        values.put(Items.SLIME_BLOCK, 225L);
-        values.put(Items.ENDER_PEARL, 20L);
-        values.put(Items.BLAZE_ROD, 30L);
-        values.put(Items.ENDER_EYE, 50L);
-        values.put(Items.BLAZE_POWDER, 15L);
-        values.put(Items.MAGMA_CREAM, 50L);
-        values.put(Items.GHAST_TEAR, 200L);
-        values.put(Items.NETHER_STAR, 2500L);
-        values.put(Items.SHULKER_SHELL, 200L);
-        values.put(Items.END_CRYSTAL, 300L);
-        values.put(Items.EXPERIENCE_BOTTLE, 100L);
-        values.put(Items.DRAGON_EGG, 2000L);
-        values.put(Items.DRAGON_HEAD, 2000L);
+        values.put(Items.SLIME_BALL, 250L);
+        values.put(Items.SLIME_BLOCK, 2250L);
+        values.put(Items.ENDER_PEARL, 200L);
+        values.put(Items.BLAZE_ROD, 300L);
+        values.put(Items.ENDER_EYE, 500L);
+        values.put(Items.BLAZE_POWDER, 150L);
+        values.put(Items.MAGMA_CREAM, 500L);
+        values.put(Items.GHAST_TEAR, 2000L);
+        values.put(Items.NETHER_STAR, 25000L);
+        values.put(Items.SHULKER_SHELL, 2000L);
+        values.put(Items.END_CRYSTAL, 3000L);
+        values.put(Items.EXPERIENCE_BOTTLE, 1000L);
+        values.put(Items.DRAGON_EGG, 20000L);
+        values.put(Items.DRAGON_HEAD, 20000L);
 
         // Decorative
-        values.put(Items.ENDER_CHEST, 140L);
+        values.put(Items.ENDER_CHEST, 1400L);
         values.put(Items.BEACON, 2500L);
         return values;
     }
